b77a1b820928a142afd310ad14afdbd7acaa200d
Eleveham <603252462@qq.om>
2024-01-08 22:28:38 +0800

    pzj-Vue

---
+++ vue/foj-vue/src/views/Answer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="a" >
+        <div id="PB">
+<el-card id="card1" class="box-card">
+  
+</el-card>
+<el-card id="card2" class="box-card">
+  
+</el-card>
+</div>
+
+   
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.a{
+    width:1440px;
+}
+ .text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
+
+  .box-card {
+    width: 480px;
+  }
+  #PB{
+    display: flex;
+  flex-direction: row;
+}
+#card1{
+
+}
+#card2{
+
+    
+}
+  
+</style>
