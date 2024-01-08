3b28ab98e67a1807a9bdab347292c0bc4d36601b
chzqz <2805089910@qq.com>
2024-01-08 14:40:37 +0800

    跨域请求处理

--- foj-springboot/foj-commons/src/main/java/icu/chzqz/foj/config/InterceptorConfig.java
+++ foj-springboot/foj-commons/src/main/java/icu/chzqz/foj/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import icu.chzqz.foj.interceptor.LoginInterceptor;
 import icu.chzqz.foj.interceptor.PermissionsInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,5 +25,19 @@ public class InterceptorConfig implements WebMvcConfigurer {
         registry.addInterceptor(permissionsInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login");
+    }
+
+    /**
+     * 跨域支持
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .maxAge(3600 * 24);
     }
 }
