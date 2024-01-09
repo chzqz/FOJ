0a732a2e44797572d7e7c69396bc2c456913738a
chzqz <2805089910@qq.com>
2024-01-09 10:49:12 +0800

    bug2: 跨域标头不能用通配符

--- foj-springboot/foj-commons/src/main/java/icu/chzqz/foj/filter/CorsFilter.java
+++ foj-springboot/foj-commons/src/main/java/icu/chzqz/foj/filter/CorsFilter.java
@@ -5,12 +5,14 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-@WebFilter(urlPatterns = {"/**"})
+@Slf4j
+@WebFilter(urlPatterns = {"/**", "/"})
 public class CorsFilter implements Filter {
 
     //销毁时候调用的方法
@@ -22,17 +24,16 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
-
+        log.info("跨域处理");
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        response.setHeader("Access-Control-Allow-Origin", " http://localhost:8081");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "content-type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         chain.doFilter(req, res);
-
     }
 
     //初始化调用的方法
